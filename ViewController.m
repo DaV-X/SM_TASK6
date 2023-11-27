31ffc2a678f4b0b6996afc925cb654cd9aaf443e
DaV <74231341+DaV-X@users.noreply.github.com>
2023-11-27 11:42:10 +0100

    Add files via upload

---
+++ ViewController.m
@@ -0,0 +1,24 @@
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *image;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [_informationButton setTitle:NSLocalizedString(@"Information",nil) forState:UIControlStateNormal];
+    [_image setImage:[UIImage imageNamed:NSLocalizedString(@"image", nil)]];
+}
+
+
+- (IBAction)act:(id)sender {
+    UIAlertController *alertDialog = [UIAlertController alertControllerWithTitle:NSLocalizedString(@"Information",nil) message:[NSString stringWithFormat:NSLocalizedString(@"The faculty is running %i programs on graduate level.", nil),4] preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action){}];
+    [alertDialog addAction:defaultAction];
+    [self presentViewController:alertDialog animated:YES completion:nil];
+}
+
+@end;
